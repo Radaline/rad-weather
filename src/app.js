e8009e27ee99b20050f685c21bb681cd4489992a
Radaline <mostreallea@gmail.com>
2022-03-16 23:34:47 -0500

    added javascript

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
+function displayTemperature(response) {
+  console.log(response.data);
+}
 
+let apiKey = "f93da01be87ea516f1ea4e13f943ea44";
+let apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=$apiKey}&units=metric";
+
+axios.get(apiUrl).then(displayTemperature);
