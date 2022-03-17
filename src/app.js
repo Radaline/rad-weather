26f1ad7fd92754034f09e4b0becbb3d65d6c5ff5
Radaline <mostreallea@gmail.com>
2022-03-17 00:27:43 -0500

    editting java

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 function displayTemperature(response) {
   console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
 }
 
 let apiKey = "f93da01be87ea516f1ea4e13f943ea44";
 let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=$apiKey}&units=metric";
+  "https://api.openweathermap.org/data/2.5/weather?q=Nashville&appid=${apiKey}&units=metric";
 
 axios.get(apiUrl).then(displayTemperature);
