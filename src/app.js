9e9357202c53781a0f6de83fdfaebba0497f7913
Radaline <mostreallea@gmail.com>
2022-03-18 01:06:45 -0500

    adding more css

--- src/app.js
+++ src/app.js
@@ -5,11 +5,15 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let windElement = document.querySelector("#windSpeed");
   let humidityElement = document.querySelector("#humidity");
+  let tempHi = document.querySelector("#highs");
+  let tempLo = document.querySelector("#lows");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].main;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  tempHi.innerHTML = Math.round(response.data.main.temp_max);
+  tempLo.innerHTML = Math.round(response.data.main.temp_min);
 }
 
 let apiKey = "f93da01be87ea516f1ea4e13f943ea44";
